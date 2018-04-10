05ee67d1fa87ffe6c00a7307550f65aa36eed425
空之境界 <34391672+ZYP1997@users.noreply.github.com>
2018-04-10 14:28:55 +0800

    Add
    Stage1;Player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform Player;
+    public float smoothing = 5f;
+
+    Vector3 offset;
+
+    void Start () {
+        offset = transform.position - Player.position;
+    }
+	
+	void FixedUpdate () {
+        Vector3 targetCamPos = Player.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public float jumpspeed;
+    private Rigidbody rg3d;
+    private ConstantForce constantforce;
+
+    // Use this for initialization
+    void Start()
+    {
+        rg3d = GetComponent<Rigidbody>();
+        constantforce = GetComponent<ConstantForce>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveX, 0f, moveZ);
+        rg3d.AddForce(movement * speed);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if(rg3d.velocity.y == 0f)
+            {
+                rg3d.velocity = new Vector3(0f, jumpspeed + 0.0001f, 0f);
+            }
+        }
+    }
+}
