79ebbdb9dfe7f542c5ea66520025023340678b21
空之境界 <34391672+ZYP1997@users.noreply.github.com>
2018-04-12 22:24:13 +0800

    Change-Stage4

---
+++ Assets/Scripts/PlatformMoving.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMoving : MonoBehaviour {
+
+    public Vector3 movement;
+    public float speed = 5f;
+    private Vector3 startpos;
+    private int dir;
+	// Use this for initialization
+	void Start () {
+        dir = 1;
+        startpos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if(transform.position ==  startpos + movement)
+        {
+            dir = 2;
+        }
+        else if(transform.position == startpos)
+        {
+            dir = 1;
+        }
+
+		if(dir == 1)
+        {
+            transform.position = Vector3.Lerp(transform.position, transform.position + movement, speed * Time.deltaTime);
+        }
+        else if(dir == 2)
+        {
+            transform.position = Vector3.Lerp(transform.position, startpos, speed * Time.deltaTime);
+        }
+	}
+}
---
+++ Assets/Scripts/RotatingBridge.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingBridge : MonoBehaviour {
+
+    public float speed = 20f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
