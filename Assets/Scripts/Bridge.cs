eac8564bcb67a6db8d2616ba15aa21957a5915cc
空之境界 <34391672+ZYP1997@users.noreply.github.com>
2018-04-12 14:01:29 +0800

    Repairs

--- Assets/Scripts/Bridge.cs
+++ Assets/Scripts/Bridge.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bridge : MonoBehaviour {
-    GameObject bridge;
     private int locks = 0;
-    private float dis = 0;
     public Vector3 targetpos;
+    public Vector3 startpos;
     public float smoothing;
     // Use this for initialization
     void Start () {
-        bridge = GameObject.Find("Bridge");
-        bridge.transform.position = new Vector3(-5.41f, -10f, 12.51f);
+        transform.position = startpos;
     }
 	
 	// Update is called once per frame
@@ -20,8 +18,7 @@ public class Bridge : MonoBehaviour {
         if (locks==1)
         {
             Vector3 vec = targetpos;
-            bridge.transform.position = Vector3.Lerp(bridge.transform.position, vec, smoothing * Time.deltaTime);
-            
+            transform.position = Vector3.Lerp(transform.position, vec, smoothing * Time.deltaTime);
         }
         
     }
