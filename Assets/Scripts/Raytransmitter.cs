ebf4371ac56856bdb054402c0f4ea2d086c77393
bhjie <34655221+bhjie@users.noreply.github.com>
2018-04-14 17:08:30 +0800

    add_scene5_ray

--- Assets/Scripts/Raytransmitter.cs
+++ Assets/Scripts/Raytransmitter.cs
@@ -6,21 +6,25 @@ public class Raytransmitter : MonoBehaviour {
 
     public float Interval = 2f;
     public float range = 100f;
-
+    public Vector3 dir;
+    public float speed;
     float timer;
     Ray shootRay = new Ray();
     RaycastHit shootHit;
     int shootableMask;
-    ParticleSystem gunParticles;
     LineRenderer gunLine;
-    AudioSource gunAudio;
-    float effectsDisplayTime = 0.2f;
-    float camRayLength = 100f;
+    public float effectsDisplayTime = 0.2f;
+
+    Rigidbody rig;
 
     // Use this for initialization
     void Start () {
         timer = 0f;
         gunLine = GetComponent<LineRenderer>();
+        shootableMask = LayerMask.GetMask("Default");
+        gunLine.SetColors(Color.black, Color.yellow);
+
+
     }
 	
 	// Update is called once per frame
@@ -28,44 +32,54 @@ public class Raytransmitter : MonoBehaviour {
         timer += Time.deltaTime;
         if (timer>=Interval)
         {
+            gunLine.enabled = false;
             Shoot();
-            timer = 0f;
-        }
-        if (timer>=Interval/2)
-        {
-            Aiming();
-        }
-        if (timer >=  effectsDisplayTime)
-        {
             DisableEffects();
+        }
+        else if (timer>=Interval/2)
+        {
+            gunLine.enabled = false;
+            Aiming();
         }
     }
     void DisableEffects()
     {
         gunLine.enabled = false;
+        timer = 0f;
     }
 
     void Shoot()
     {
-        gunLine.enabled = true;
-        gunLine.SetPosition(0, transform.position);
-        shootRay.origin = transform.position;
-        shootRay.direction = transform.forward;
+        Object Bullet = Resources.Load("Perfabs/Bullet", typeof(GameObject));
+        GameObject bullet = Instantiate(Bullet) as GameObject;
+        bullet.transform.position = transform.position;
+        bullet.name = "bullet_1";
+        rig = bullet.GetComponent<Rigidbody>();
+        rig.velocity = dir * speed;
 
-        gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
     }
 
     void Aiming()
     {
         var colorbegin = Color.white;
-        var colorend = new Color(1, timer / 2, 1, 0);
+        var colorend = new Color(1, timer / 2, 1, 1);
+
+        
         gunLine.enabled = true;
         gunLine.SetPosition(0, transform.position);
-        gunLine.startColor = colorbegin;
-        gunLine.endColor = colorend;
         shootRay.origin = transform.position;
         shootRay.direction = transform.forward;
+        if (Physics.Raycast(shootRay, out shootHit, range, shootableMask))
+        {
 
-        gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+            gunLine.SetPosition(1, shootHit.point);
+
+        }
+        else
+        {
+
+            gunLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
+        }
+
     }
 }
