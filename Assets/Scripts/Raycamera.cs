fcbe869c3940f084603bbf53e35864a1734b37fa
bhjie <34655221+bhjie@users.noreply.github.com>
2018-04-16 23:24:22 +0800

    fin_scene5

---
+++ Assets/Scripts/Raycamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycamera : MonoBehaviour {
+    private GameObject Player;
+    private int flag = 0;
+    public float smoothing = 5f;
+    public float dis;
+    Vector3 targetCamPos;
+    Vector3 offset;
+    Vector3 rot;
+    Rigidbody rig;
+    // Use this for initialization
+    void Start () {
+        Player = GameObject.Find("Player");
+        rig = Player.GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Player == null)
+        {
+            Player = GameObject.Find("Player");
+            rig = Player.GetComponent<Rigidbody>();
+        }
+        offset = -rig.velocity.normalized * dis;
+        transform.forward = Player.transform.position - transform.position;
+        targetCamPos = Player.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Scripts/SwapCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapCamera : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject[] cameras;
+    public string[] shotcuts;
+    public bool changeAudioListener = true;
+    private int cflag = 0;
+
+    private void Start()
+    {
+        cameras[1].GetComponent<AudioListener>().enabled = false;
+        cameras[1].GetComponent<Camera>().enabled = false;
+    }
+    void Update()
+    {
+        
+        if (Input.GetKeyUp("q"))
+            SwitchCamera();
+    }
+
+    void SwitchCamera()
+    {
+        cameras[cflag].GetComponent<AudioListener>().enabled = false;
+        cameras[cflag].GetComponent<Camera>().enabled = false;
+        cflag = 1 - cflag;
+        cameras[cflag].GetComponent<AudioListener>().enabled = true;
+        cameras[cflag].GetComponent<Camera>().enabled = true;
+        
+    }
+}
